27b5acebef68af2be047c756503c4a98508ebf3d
dardavar <33800735+dardavar@users.noreply.github.com>
2021-01-20 10:57:09 +0200

    Add files via upload

---
+++ FFElement.py
@@ -0,0 +1,68 @@
+import FFUtils
+
+class FFElement:
+
+    def __init__(self, value, characteristic=7):
+        self.value = value
+        self.characteristic = characteristic
+    
+    def AddInverse(self):
+        if(self.value==0):
+            return FFElement(0)
+        return FFElement(self.characteristic - self.value)
+    
+    def MulInverse(self):
+        try:
+            if(self.value == 0):
+                raise ZeroDivisionError
+            return FFElement(FFUtils.ExtendedEuclideanAlgorithm(self.value, self.characteristic)[1])          
+        except ZeroDivisionError:
+            print ("Zero have no inverse, returning -1")
+            return FFElement(-1)
+    
+    def AddOrder(self):
+        if(self.value==0):
+            return 1
+        return self.characteristic
+
+    def Add(self, other):
+        return FFElement((self.value + other.value) % self.characteristic)
+    
+    def Sub(self, other):
+        return FFElement((self.value + other.AddInverse().value) % self.characteristic)
+    
+    def Mul(self, other):
+        return FFElement((self.value * other.value) % self.characteristic)
+    
+    #get the multlipical inverse and multiply by it
+    def Div(self, other):
+        return self.Mul(other.MulInverse())
+
+    def AddPower(self,power):
+        return FFElement((self.value*power)%self.characteristic)
+
+    def MulPower(self,power):
+        return FFElement(FFUtils.SuperPower(self.value, power, self.characteristic-1))
+    
+    def MulOrder(self, special = True):
+        #define o(0) as -1
+        if(self.value==0):
+            return -1
+        elif(self.value==1):
+            return 1
+        #if (p-1)/2 is prime, then its the only number needs to be checked
+        elif(special):
+            if((self.MulPower((self.characteristic-1)/2)).value==1):
+                return (self.characteristic-1)/2
+        #if (p-1)/2 isn't prime, check every divisor of (p-1)/2
+        else:
+            for i in range(2,self.characteristic):
+                if(self.characteristic%i==0):
+                    if (self.MulPower(i).value==1):
+                        return i
+        return self.characteristic - 1
+            
+
+    
+
+        
---
+++ FFUtils.py
@@ -0,0 +1,19 @@
+#computes a^b mod n efficiently 
+def SuperPower(a,b,n):
+    if (b==0):
+        return 1
+    elif (b%2==1):
+        return (a*SuperPower(a,b-1,n)%n)
+    else:
+        a = SuperPower(a,b/2,n)
+        return (a*a)%n
+
+#returns the gcd of a and b, and x1, x2 s.t. gcd(a,b)=x*a+y*b.
+def ExtendedEuclideanAlgorithm(a,b):
+    if(a==0):
+        return b,0,1
+    gcd,x1,y1 = ExtendedEuclideanAlgorithm(b%a, a)
+
+    x = y1 - (b//a)*x1
+    y = x1
+    return gcd, x, y
