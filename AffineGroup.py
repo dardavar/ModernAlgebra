110c1f0b22126d2a530d65c348d1a43bb5fa4af3
Almog Zadik <almogz10@gmail.com>
2021-03-17 17:51:37 +0200

    add min doc (not completed yet)

--- AffineGroup.py
+++ AffineGroup.py
@@ -5,18 +5,20 @@ from AffineElement import AffineElement
 
 class AffineGroup:
 
+    # Class of affine group, each object consist of a, b and the field finite number characteristic
     def __init__(self, elliptic_curve):
-
         self.a = elliptic_curve.a
         self.b = elliptic_curve.b
         self.p = elliptic_curve.p
         self.order = len(self.generate_all_points())
 
+    # print all group supportive points
     def print_all_points(self):
         points = self.generate_all_points()
         for point in points:
             print(point)
 
+    # generate list of the group supportive points which holds the equation of y^2=x^3+a*x+b
     def generate_all_points(self):
         points = [self.generate_identity_element()]
         for x_value in range(0, self.p):
